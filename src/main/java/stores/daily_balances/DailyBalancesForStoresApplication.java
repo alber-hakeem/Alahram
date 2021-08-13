9163d05a6568df2b592d4dc6dbb14801f40cd2f6
Alber Rashad <alber.rashad@ntsal.com>
2021-08-13 23:31:32 +0200

    add postgres

--- src/main/java/stores/daily_balances/DailyBalancesForStoresApplication.java
+++ src/main/java/stores/daily_balances/DailyBalancesForStoresApplication.java
@@ -29,60 +29,60 @@ public class DailyBalancesForStoresApplication {
         SpringApplication.run(DailyBalancesForStoresApplication.class, args);
     }
 
-    @PostConstruct
-    public void initBalances() throws FileNotFoundException, URISyntaxException {
-
-        Scanner scan = new Scanner(streamToFile(getClass().getClassLoader().getResourceAsStream("static/csv/TotalBalancesOfAugust.csv")));
-        ArrayList<String[]> records = new ArrayList<>();
-        String[] record;
-        while(scan.hasNext()) {
-            record = scan.nextLine().split(",");
-            records.add(record);
-        }
-
-        Collection<Product> productCollection = new ArrayList<>();
-        for(String[] temp : records) {
-            Product balance = new Product();
-            int counter = 0;
-            for(String temp1 : temp) {
-                if (counter == 0) balance.setId(Integer.parseInt(temp1));
-                else if (counter == 1) balance.setName(temp1);
-                else if (counter == 2) balance.setBadr(temp1);
-                else if (counter == 3) balance.setBasos(temp1);
-                else if (counter == 4) balance.setAlex(temp1);
-                else if (counter == 5) balance.setTotal(temp1);
-                else if (counter == 6) balance.setMerchantPrice(temp1);
-                else if (counter == 7) balance.setFactoryPrice(temp1);
-                else if (counter == 8) balance.setType(temp1);
-                else balance.setCode(temp1);
-
-                counter++;
-            }
-            productCollection.add(balance);
-        }
-        productRepository.saveAll(productCollection);
-    }
-    public static File streamToFile(InputStream in) {
-        if (in == null) {
-            return null;
-        }
-
-        try {
-            File f = File.createTempFile(String.valueOf(in.hashCode()), ".tmp");
-            f.deleteOnExit();
-
-            FileOutputStream out = new FileOutputStream(f);
-            byte[] buffer = new byte[1024];
-
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
-
-            return f;
-        } catch (IOException e) {
-          //  LOGGER.error(e.getMessage(), e);
-            return null;
-        }
-    }
+//    @PostConstruct
+//    public void initBalances() throws FileNotFoundException, URISyntaxException {
+//
+//        Scanner scan = new Scanner(streamToFile(getClass().getClassLoader().getResourceAsStream("static/csv/TotalBalancesOfAugust.csv")));
+//        ArrayList<String[]> records = new ArrayList<>();
+//        String[] record;
+//        while(scan.hasNext()) {
+//            record = scan.nextLine().split(",");
+//            records.add(record);
+//        }
+//
+//        Collection<Product> productCollection = new ArrayList<>();
+//        for(String[] temp : records) {
+//            Product balance = new Product();
+//            int counter = 0;
+//            for(String temp1 : temp) {
+//                if (counter == 0) balance.setId(Integer.parseInt(temp1));
+//                else if (counter == 1) balance.setName(temp1);
+//                else if (counter == 2) balance.setBadr(temp1);
+//                else if (counter == 3) balance.setBasos(temp1);
+//                else if (counter == 4) balance.setAlex(temp1);
+//                else if (counter == 5) balance.setTotal(temp1);
+//                else if (counter == 6) balance.setMerchantPrice(temp1);
+//                else if (counter == 7) balance.setFactoryPrice(temp1);
+//                else if (counter == 8) balance.setType(temp1);
+//                else balance.setCode(temp1);
+//
+//                counter++;
+//            }
+//            productCollection.add(balance);
+//        }
+//        productRepository.saveAll(productCollection);
+//    }
+//    public static File streamToFile(InputStream in) {
+//        if (in == null) {
+//            return null;
+//        }
+//
+//        try {
+//            File f = File.createTempFile(String.valueOf(in.hashCode()), ".tmp");
+//            f.deleteOnExit();
+//
+//            FileOutputStream out = new FileOutputStream(f);
+//            byte[] buffer = new byte[1024];
+//
+//            int bytesRead;
+//            while ((bytesRead = in.read(buffer)) != -1) {
+//                out.write(buffer, 0, bytesRead);
+//            }
+//
+//            return f;
+//        } catch (IOException e) {
+//          //  LOGGER.error(e.getMessage(), e);
+//            return null;
+//        }
+//    }
 }
